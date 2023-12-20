40ed9da89cebcd4eae167398ec8870986fa189c8
JoseMoreno00 <accgitjosem@gmail.com>
2023-12-20 13:42:00 -0800

    First Commit

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    
+    usrID = int(argv[1])
+    usrDATA = requests.get(f"{url}users/{usrID}").json()
+    usrTASKS = requests.get(f"{url}users/{usrID}/todos").json()
+    usrcompleteTask = []
+
+    for task in usrTASKS:
+        if task["completed"]:
+            usrcompleteTask.append(task)
+
+    print(f"Employee {usrDATA['name']} is done with ", end="")
+    print(f"tasks({len(usrcompleteTask)}/{len(usrTASKS)}):")
+
+    for task in usrcompleteTask:
+        print(f"\t {task['title']}")
